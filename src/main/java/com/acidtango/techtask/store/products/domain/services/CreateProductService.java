48ea10777090b1eaa56f6c6a04b326d39a78f9e0
Axel <axelmc@gmail.com>
2023-06-02 16:31:04 +0200

    CreateProductService now extends DomainService

--- src/main/java/com/acidtango/techtask/store/products/domain/services/CreateProductService.java
+++ src/main/java/com/acidtango/techtask/store/products/domain/services/CreateProductService.java
@@ -1,5 +1,6 @@
 package com.acidtango.techtask.store.products.domain.services;
 
+import com.acidtango.techtask.common.domain.DomainService;
 import com.acidtango.techtask.store.products.domain.models.entities.Product;
 import com.acidtango.techtask.store.products.domain.repositories.ProductRepository;
 import org.bson.types.ObjectId;
@@ -7,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CreateProductService {
+public class CreateProductService extends DomainService {
 
     private final ProductRepository productRepository;
 
